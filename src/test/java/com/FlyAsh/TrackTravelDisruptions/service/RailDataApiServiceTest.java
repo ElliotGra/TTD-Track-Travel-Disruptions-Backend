ba7efc1f3235024dc88cfa9d74ae02913da782ab
Jasmanjot <158616491+Jimmyrai321@users.noreply.github.com>
2025-08-09 00:36:37 +0100

    Revert "Docker"

--- src/test/java/com/FlyAsh/TrackTravelDisruptions/service/RailDataApiServiceTest.java
+++ src/test/java/com/FlyAsh/TrackTravelDisruptions/service/RailDataApiServiceTest.java
@@ -2,20 +2,15 @@ package com.FlyAsh.TrackTravelDisruptions.service;
 
 import com.FlyAsh.TrackTravelDisruptions.dto.RailDataDTO;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-
-import static reactor.core.publisher.Mono.when;
-
 class RailDataApiServiceTest {
+
 
 
 
     @Test
     void getNextFastestServiceBetween() {
-        // Test require API key
-
-//        RailDataApiService railDataApiService = new RailDataApiService();
-//        RailDataDTO railDataDTO = railDataApiService.getNextFastestServiceBetween("BHM", "WVH", 90);
-//        System.out.println(railDataDTO.toString());
+        RailDataApiService railDataApiService = new RailDataApiService();
+        RailDataDTO railDataDTO = railDataApiService.getNextFastestServiceBetween("RDG", "PGN", 90);
+        System.out.println(railDataDTO.toString());
     }
 }
